c91dd75f3e6c33eaa02f4fd9959fb71c8bcf580e
natsurainko <a-275@qq.com>
2023-04-29 20:42:10 +0800

    初始化工作

---
+++ Omms.Client/Views/DashboardPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace Omms.Client.Views;
+
+public sealed partial class DashboardPage : Page
+{
+    public DashboardPage()
+    {
+        this.InitializeComponent();
+    }
+
+    private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+    {
+        var rightGrid = RightGrid;
+
+        if (e.NewSize.Width < 850)
+        {
+            rightGrid.Padding = new Thickness(0, 0, 0, 0);
+            if (rightGrid.Parent is Grid)
+            {
+                grid.Children.Remove(rightGrid);
+                stackPanel.Children.Add(rightGrid);
+            }
+        }
+        else
+        {
+            rightGrid.Padding = new Thickness(40, 0, 0, 0); 
+            if (rightGrid.Parent is StackPanel)
+            {
+                stackPanel.Children.Remove(rightGrid);
+                grid.Children.Add(rightGrid);
+            }
+        }
+    }
+}
